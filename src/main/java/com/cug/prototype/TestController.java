a6c58f95dd6d9b3ed8e38fc93a6498c7c221f050
88471 <1778402372@qq.com>
2021-10-23 21:33:56 +0800

    init

---
+++ src/main/java/com/cug/prototype/TestController.java
@@ -0,0 +1,15 @@
+package com.cug.prototype;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+    @GetMapping
+    @ResponseBody
+    public String test() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/cug/prototype/PrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cug.prototype;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrototypeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
