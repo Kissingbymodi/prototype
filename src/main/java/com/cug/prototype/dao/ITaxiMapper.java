eeecc066605db2fb55f42f7af10a00e188947b41
lenovo <yangrlzzu98@163.com>
2021-10-29 20:49:23 +0800

    init

--- src/main/java/com/cug/prototype/dao/ITaxiMapper.java
+++ src/main/java/com/cug/prototype/dao/ITaxiMapper.java
@@ -1,9 +1,10 @@
 package com.cug.prototype.dao;
 
 import com.cug.prototype.entity.TaxiEntity;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface ITaxiMapper {
 
     List<TaxiEntity> queryAllTaxi();
